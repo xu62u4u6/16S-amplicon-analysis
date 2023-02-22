acd37506e5fc196d76815092b82767e526ff5088
blueorange <199928ltyos@gmail.com>
2023-02-23 06:10:40 +0800

    remove cd

--- sh/setup_blastplus.sh
+++ sh/setup_blastplus.sh
@@ -1,10 +1,9 @@
-echo "1.download blast+"
-cd ~
+echo "1.download blast+..."
 wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz -P ~
 tar -zxvf ncbi-blast-*.tar.gz -C ~
 export PATH=~/ncbi-blast-2.13.0+/bin:$PATH
 rm ~/ncbi-blast-2.13.0+-x64-linux.tar.gz
-echo "2.download 16s database"
+echo "2.download 16s database..."
 mkdir ~/blast_db
 wget https://ftp.ncbi.nlm.nih.gov/blast/db/16S_ribosomal_RNA.tar.gz -P ~/blast_db
 tar -zxvf ~/blast_db/16S_ribosomal_RNA.tar.gz -C ~/blast_db
