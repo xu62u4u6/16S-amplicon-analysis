0819edecde99dcab85796969a8cadaf1a9c2b163
blueorange <199928ltyos@gmail.com>
2023-02-23 06:06:35 +0800

    fix path bug

--- sh/setup_blastplus.sh
+++ sh/setup_blastplus.sh
@@ -1,7 +1,11 @@
+echo "1.download blast+"
 cd ~
-wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz
-tar -zxvf ncbi-blast-*.tar.gz
+wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz -P ~
+tar -zxvf ncbi-blast-*.tar.gz -C ~
 export PATH=~/ncbi-blast-2.13.0+/bin:$PATH
-mkdir blast_db && cd blast_db
-wget https://ftp.ncbi.nlm.nih.gov/blast/db/16S_ribosomal_RNA.tar.gz
-tar -zxvf 16S_ribosomal_RNA.tar.gz
+rm ~/ncbi-blast-2.13.0+-x64-linux.tar.gz
+echo "2.download 16s database"
+mkdir ~/blast_db
+wget https://ftp.ncbi.nlm.nih.gov/blast/db/16S_ribosomal_RNA.tar.gz -P ~/blast_db
+tar -zxvf ~/blast_db/16S_ribosomal_RNA.tar.gz -C ~/blast_db
+rm ~/blast_db/16S_ribosomal_RNA.tar.gz
